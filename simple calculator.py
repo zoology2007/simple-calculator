a8b18a5d71b0d2860e96f0cbc2d27f921b90313d
zoology2007 <90775711+zoology2007@users.noreply.github.com>
2021-09-18 12:39:09 +0530

    Add files via upload

---
+++ simple calculator.py
@@ -0,0 +1,13 @@
+print("simple calculator")
+a = 100
+b = 110
+sum = a + b
+subtract = a - b
+divide = a / b
+reminder = a % b
+multiply = a * b
+print("sum is",sum)
+print("difference is",subtract)
+print("quotient is",divide)
+print("reminder is",reminder)
+print("product is",multiply)
